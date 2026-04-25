d7eeca725e12d009b7151035d236b76322bbe44c
Lucas NG <leeboykt@gmail.com>
2026-04-25 03:54:06 -0400

    format output

--- emotion_detection.py
+++ emotion_detection.py
@@ -11,7 +11,36 @@ def emotion_detector(text_to_analyze) -> str:
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_data = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, data=json.dumps(input_data), headers=headers)
-    return response.text
+    formatted_data = json.loads(response.text)
+    # get the emotion value
+    emotion = formatted_data.get("emotionPredictions")[0].get("emotion")
+    # get the score
+    anger_score = emotion.get("anger")
+    disgust_score = emotion.get("disgust")
+    fear_score = emotion.get("fear")
+    joy_score = emotion.get("joy")
+    sadness_score = emotion.get("sadness")
+    # get the dominant_emotion by using max filter
+    emotion_dict = {
+            'anger': anger_score,
+            'disgust': disgust_score,
+            'fear': fear_score,
+            'joy': joy_score,
+            'sadness': sadness_score,
+    }
+    max_value = max(emotion_dict.values())
+    dominant_emotion = ""
+    for key in emotion_dict.keys():
+        if emotion_dict[key] == max_value:
+            dominant_emotion = key
+    return {
+            'anger': anger_score,
+            'disgust': disgust_score,
+            'fear': fear_score,
+            'joy': joy_score,
+            'sadness': sadness_score,
+            'dominant_emotion': dominant_emotion
+            }
 
 
 # test
